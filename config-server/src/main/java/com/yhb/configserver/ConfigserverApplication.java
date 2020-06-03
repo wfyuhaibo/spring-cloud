11f40ec81a35621a898bfdd9bc46c9cf521faa82
Yuhai <yuhaibo@a-city.cn>
2020-06-03 18:45:05 +0800

    添加config

---
+++ config-server/src/main/java/com/yhb/configserver/ConfigserverApplication.java
@@ -0,0 +1,21 @@
+package com.yhb.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 统一消息配置中心
+ *
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigserverApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
